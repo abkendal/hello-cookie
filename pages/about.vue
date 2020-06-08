1ef202a329675ad2058442fed2e425fc98f2d218
Adam Kendal <adam.kendal@nofixedaddressinc.com>
2020-06-08 16:53:04 -0400

    Add Uber eats menu

--- pages/about.vue
+++ pages/about.vue
@@ -9,7 +9,7 @@
 			<div class="container">
 				<h1 class="body__headline">Toronto's best gourmet fresh cookie caterer</h1>
 				<div class="body__inner">
-					<p>As Toronto's premier fresh cookie caterer, Hello Cookie provides a magical unique experience that will have your guests talking! Our team will sweeten your day and make your event something truly memorable with our delicious fresh cookie service.</p>
+					<p>As Toronto's premier fresh cookie caterer, Hello Cookie provides a magical unique experience that will have your guests talking! Out of the heart of the Annex, our team will sweeten your day and make your event something truly memorable with our delicious fresh cookie service.</p>
 					<p>We cater corporate functions and social events including catering for weddings, receptions, baptisms, baby showers, birthdays, anniversaries, and more. Hello Cookie is offers a wide array of customizable packages to suit the unique needs of your event.</p>
 				</div>
 			</div>
---
+++ pages/menu.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="about menu">
+        <div class="hero">
+            <div class="desktop-container">
+                <img src="~assets/images/home-hero.jpg" alt="">
+            </div>
+        </div>
+        <div class="body">
+            <div class="container">
+                <h1 class="body__headline">Uber Eats Menu</h1>
+                <div class="body__inner">
+                    <div class="body__row">
+                        <div class="body__item">
+                            <h2>Fresh Baked Cookies - 1/2 Dozen (6)</h2>
+                            <p>Choose from: OG Chocolate Chip, Chocolate Chip Deluxe, Cookies & Cream, Peanutbutter cup</p>
+                        </div>
+                        <div class="body__price">$15</div>
+                    </div>
+                    <div class="body__row">
+                        <div class="body__item">
+                            <h2>Fresh Baked Cookies - Full Dozen (12)</h2>
+                            <p>Choose from: OG Chocolate Chip, Chocolate Chip Deluxe, Cookies & Cream, Peanutbutter cup</p>
+                        </div>
+                        <div class="body__price">$28</div>
+                    </div>
+                    <div class="body__row">
+                        <div class="body__item">
+                            <h2>Assorted Fresh Baked Cookies - 1/2 Dozen (6)</h2>
+                            <p>An assortment of 6 of our gourmet cookies freshly baked just for you.</p>
+                        </div>
+                        <div class="body__price">$15</div>
+                    </div>
+                    <div class="body__row">
+                        <div class="body__item">
+                            <h2>Assorted Fresh Baked Cookies - Full Dozen (12)</h2>
+                            <p>An assortment of 12 of our gourmet cookies freshly baked just for you.</p>
+                        </div>
+                        <div class="body__price">$28</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	
+</template>
